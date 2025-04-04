488dcc08116289e38276d05e08d36a3c69601c89
Max Petretta <m@maxp.co>
2025-04-04 18:28:58 -0400

    initial commit

--- src/index.ts
+++ src/index.ts
@@ -5,3 +5,20 @@
  * Once you have a minimal reproduction, commit it and push it to a public
  * GitHub repo.
  */
+
+export async function main() {
+	// Existing code
+	const num = Math.random();
+	if (num > 0.5) {
+		console.log("Greater than 0.5");
+	}
+
+	// Add a new check
+	// TODO: try removing and re-adding closing paren
+	if (num < 0.5 && num < 0.4) {
+		console.log("Less than 0.5");
+	}
+
+	// Existing return
+	return num;
+}
